742f7d723fbd0e4dbaecf5709215ff1ac59899a4
ison <isonz@qq.com>
2020-10-04 23:03:54 +0800

    asf

--- src/components/my/my-update.js
+++ src/components/my/my-update.js
@@ -2,22 +2,53 @@
 
 const dynamoDb = require('../../helpers/dynamodb');
 const config = require('./base');
+const authId = require( '../../helpers/auth-id');
+
 
 module.exports = (event, callback) => {
-  const data = JSON.parse(event.body);
-
-  data.id = event.pathParameters.id;
-  data.updatedAt = new Date().getTime();
-
   const params = {
-    TableName : config.TableName,
-    Item: data
+    TableName: config.TableName,
+    Key: {
+      id: authId(event, callback),
+    }
   };
 
-  return dynamoDb.put(params, (error, data) => {
+  return dynamoDb.get(params, (error, result) => {
     if (error) {
       callback(error);
+      return null;
     }
-    callback(error, params.Item);
+
+    const updateItem = JSON.parse(event.body);
+    result.Item.updatedAt = new Date().getTime();
+    const allData = Object.assign(
+        result.Item,
+        updateItem,
+    );
+
+    const putParams = {
+      TableName : config.TableName,
+      Item: allData
+    };
+
+    dynamoDb.put(putParams, (error, data) => {
+      if (error) {
+        callback(error);
+        return null;
+      }
+
+      const item = {
+        avatar: allData.avatar,
+        createdAt: allData.createdAt,
+        email: allData.email,
+        id: allData.id,
+        username: allData.username,
+      };
+
+      callback(error, item);
+    });
   });
 };
+
+
+
